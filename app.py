77502e8cd6bd41b86a2cd0ae6f0ab978c1225d87
Ben McNeill <ben@activeprogrammer.com>
2019-07-14 15:40:15 -0600

    finalized part2

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-
+import os
+from pprint import pprint as pp
 
 def main():    
     clear_stout()
@@ -6,11 +7,33 @@ def main():
     
 
 def print_menu():        
-    pass
+    """
+    Make a selection
+    1 - option1
+    2 - option2
+    3 - option3
+    """
+
+    db = []
+    row1 = {"row_num":1,"title":"Value Error"}
+    db.append(row1)
+
+    row2 = {"row_num":2,"title":"Key Error"}
+    db.append(row2)
+    
+    row3 = {"row_num":3,"title":"KeyBoard Error"}
+    db.append(row3)
+   
+    print("Make a selection")    
+    for row in db:
+        row_num = row.get('row_num')
+        title = row.get('title')
+        menu_option = "{} - {}".format(row_num,title)
+        print(menu_option)
 
 
 def clear_stout():
-    pass
+    os.system('cls' if os.name == 'nt' else "printf '\033c'")
 
 
 if __name__ == "__main__":
